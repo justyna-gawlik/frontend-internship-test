b716356c0a7c677e9105633020c7d00ed45b2763
Justyna Gawlik <63655667+justyna-gawlik@users.noreply.github.com>
2021-02-20 15:36:10 +0100

    Task 2
    Add form validation

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,33 @@
+const email=document.getElementById("email");
+const password=document.getElementById("password");
+const form=document.getElementById("login");
+const check=document.getElementById('terms');
+const error=document.getElementById("error");
+
+form.addEventListener("submit", (e)=>{
+    let text;
+    e.preventDefault();
+
+    if(email.value===''||email.value==null){
+        text="Email is required!";
+        error.innerHTML=text;
+        return false;
+    }
+    if(password.value===''||email.value==null){
+        text="Password is required!";
+        error.innerHTML=text;
+        return false;
+    }
+
+    if(check.checked==false){
+        text="Agreement is required!";
+        error.innerHTML=text;
+        return false;
+    }
+    text="";
+    error.innerHTML=text;
+})
+
 document.getElementById('show-popup-form').addEventListener("click",()=>{
     document.getElementById('popup').style.display="flex";
 });
@@ -5,3 +35,6 @@ document.getElementById('show-popup-form').addEventListener("click",()=>{
 document.getElementById("close").addEventListener('click',()=>{
     document.getElementById('popup').style.display="none";
 });
+
+
+
