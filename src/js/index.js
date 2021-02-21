cffd32cc56f31d74f8527ea389efc3d5c0535ef2
Justyna Gawlik <63655667+justyna-gawlik@users.noreply.github.com>
2021-02-21 11:27:55 +0100

    Task 3
    Add success message function

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,15 @@ const password=document.getElementById("password");
 const form=document.getElementById("login");
 const check=document.getElementById('terms');
 const error=document.getElementById("error");
+const successMessage=document.getElementById('success-message');
+
+document.getElementById('show-popup-form').addEventListener("click",()=>{
+    document.getElementById('popup').style.display="flex";
+});
+
+document.getElementById("close").addEventListener('click',()=>{
+    document.getElementById('popup').style.display="none";
+});
 
 form.addEventListener("submit", (e)=>{
     let text;
@@ -18,23 +27,23 @@ form.addEventListener("submit", (e)=>{
         error.innerHTML=text;
         return false;
     }
-
     if(check.checked==false){
         text="Agreement is required!";
         error.innerHTML=text;
         return false;
     }
-    text="";
-    error.innerHTML=text;
-})
-
-document.getElementById('show-popup-form').addEventListener("click",()=>{
-    document.getElementById('popup').style.display="flex";
+    showMessage();
 });
 
-document.getElementById("close").addEventListener('click',()=>{
-    document.getElementById('popup').style.display="none";
-});
+function showMessage(){
+    setTimeout(()=>{
+        document.getElementById('popup').style.display="none";
+        document.getElementById('show-popup-form').style.display="none";
+        successMessage.style.display="block";
+        successMessage.innerHTML="Thank you!";
+    },3000);
+}
+
 
 
 
